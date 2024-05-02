d917236f9e355a489884704b93d212233c85b826
YEAHx4 <developerstarlight@gmail.com>
2024-05-03 01:19:36 +0900

    main page done

--- components/DetailDropdown.tsx
+++ components/DetailDropdown.tsx
@@ -4,6 +4,9 @@ import { useState } from "react";
 
 export default function DetailDropdown() {
   const [isDown, setIsDown] = useState(false);
+  const [img1, setImg1] = useState(false);
+  const [img2, setImg2] = useState(false);
+  const [img3, setImg3] = useState(false);
 
   return (
     <div className="w-[300px] mt-16">
@@ -37,9 +40,41 @@ export default function DetailDropdown() {
           }}
           className="gap-4 flex flex-col"
         >
-          <img className="mt-4" src="/img/dropdown/dropdown1.png" />
-          <div className="mt-[-1rem] text-center">
+          <img
+            onClick={() => setImg1(!img1)}
+            className="mt-4"
+            src={
+              img1
+                ? "/img/dropdown/dropdown1-2.png"
+                : "/img/dropdown/dropdown2.png"
+            }
+          />
+          <div className="mt-[-0.5rem] text-center">
+            {img1 ? `" 칭찬받았다♪ "` : `" 드디어 웃어 줬어. "`}
+          </div>
+          <img
+            onClick={() => setImg2(!img2)}
+            className="mt-4"
+            src={
+              img2
+                ? "/img/dropdown/dropdown2-2.png"
+                : "/img/dropdown/dropdown1.png"
+            }
+          />
+          <div className="mt-[-0.5rem] text-center">
             " 우리 둘만의 숨은 피니로 하자! "
+          </div>
+          <img
+            onClick={() => setImg3(!img3)}
+            className="mt-4"
+            src={
+              img3
+                ? "/img/dropdown/dropdown3-2.png"
+                : "/img/dropdown/dropdown3.png"
+            }
+          />
+          <div className="mt-[-0.5rem] text-center">
+            {img3 ? `" 내가 지켜 줄게 "` : ``}
           </div>
         </div>
       )}
