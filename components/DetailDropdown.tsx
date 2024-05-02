cd4e184c8aeafbdb0ccc2a098ba5dac3232dce45
YEAHx4 <developerstarlight@gmail.com>
2024-05-03 01:07:33 +0900

    main page img

--- components/DetailDropdown.tsx
+++ components/DetailDropdown.tsx
@@ -9,24 +9,38 @@ export default function DetailDropdown() {
     <div className="w-[300px] mt-16">
       <div className="hover:cursor-pointer" onClick={() => setIsDown(!isDown)}>
         <div className="w-full flex h-[350px] justify-between">
-          <img src="/img/ann.webp" alt="ann" className="" />
-          <img src="/img/kohane.webp" alt="kohane" />
+          <img
+            src="/img/ann.webp"
+            alt="ann"
+            className=""
+            style={{
+              animationName: "fadeLeftIn",
+              animationDuration: "1s",
+            }}
+          />
+          <img
+            src="/img/kohane.webp"
+            alt="kohane"
+            style={{
+              animationName: "fadeRightIn",
+              animationDuration: "1s",
+            }}
+          />
         </div>
         <div className="text-center mt-4">{isDown ? "접기" : "펼치기"}</div>
       </div>
       {isDown && (
-        <div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
+        <div
+          style={{
+            animationName: "fadeDownIn",
+            animationDuration: "0.5s",
+          }}
+          className="gap-4 flex flex-col"
+        >
+          <img className="mt-4" src="/img/dropdown/dropdown1.png" />
+          <div className="mt-[-1rem] text-center">
+            " 우리 둘만의 숨은 피니로 하자! "
+          </div>
         </div>
       )}
     </div>
