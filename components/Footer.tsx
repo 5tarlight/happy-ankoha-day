0448c70107e421b3c87ffbc03b19cf1cf69937c5
YEAHx4 <developerstarlight@gmail.com>
2024-12-08 22:18:13 +0900

    푸터 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="flex w-full justify-center mt-16">
-      <div className="w-[80%] h-56 border-t-2 border-white mt-4 max-w-[500px] flex flex-col items-center justify-center">
+      <div className="w-[80%] h-64 border-t-2 border-white mt-4 max-w-[500px] flex flex-col items-center justify-center">
         <div className="mb-2">&copy; 2024 AnKoha Day.</div>
         <div className="text-center">
           This page is not endorsed by Sega and not associated with Project
@@ -11,6 +11,13 @@ export default function Footer() {
         <div className="text-center">
           Project Sekai &copy; Sega | Colorful Palette | Crypton Future Media{" "}
         </div>
+        <a
+          className="mt-4 underline"
+          href="https://github.com/5tarlight/happy-ankoha-day"
+          target="_blank"
+        >
+          Github
+        </a>
       </div>
     </div>
   );
